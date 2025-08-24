c542738a0077e19d7d8fe35c36289860b26fcbc5
guadalupe <ortizguadalupedelvalle30@gmail.com>
2025-08-24 15:50:25 -0300

    primer commit

---
+++ src/Informe/md.java
@@ -0,0 +1,5 @@
+#Informe TP  N°6
+*Complicaciones: tuve complicaciones en armar la estructura del codigo, o sea, diferenciar entre las clases y la manera
+de armar.
+*Herramientas a las que requeri/use: en la parte de "teoria" como separar abstrac class y clases use chat gp y youtube
+*Si tuve inconvenientes en armar los tres programas.
